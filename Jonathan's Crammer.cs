f48d57a972dcb87c049a1606734917d25ffd626a
Hendricks17 <87331856+Hendricks17@users.noreply.github.com>
2021-07-12 18:57:46 +0100

    Add files via upload

---
+++ Jonathan's Crammer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace jonathan_s_crammer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a,b,c,d,e and f");//asks the user to input values for a,b,c,d,e,f
+            double a,b,c,d,e,f;//declare the variables a,b,c,d,e,f
+            a = Convert.ToDouble(Console.ReadLine());//initialize the variable a
+            b = Convert.ToDouble(Console.ReadLine());//initialize the variable b
+            c = Convert.ToDouble(Console.ReadLine());//initialize the variable c
+            d = Convert.ToDouble(Console.ReadLine());//initialize the variable d
+            e = Convert.ToDouble(Console.ReadLine());//initialize the variable e
+            f = Convert.ToDouble(Console.ReadLine());//initialize the variable f
+            double g = (a * d) - (b * c);//calculation for DETERMINANT
+            double h = (e * d) - (b * f);//calculation for Determinant X
+            double i = (a * f) - (e * c);//calculation for Determinant Y
+            double x = h / g;//calculation for X
+            double y = i / g;//calculation for Y
+            if (g == 0)
+            Console.WriteLine("the equation has no solution");
+            else
+            Console.Write("x is " + x);
+            Console.WriteLine(" and y is " + y);
+        
+    }
+}
+}
---
+++ Jonathan's Student Questionnaire.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("are you a full time or partime student?");//asking if the student is full time or partime
+            string student = Convert.ToString (Console.ReadLine());//delaring and initialiing "student" as a variable
+            if (student == "full time")
+           { 
+            Console.WriteLine("what is your course of study?");//asks for student's course of study
+            string x = Convert.ToString(Console.ReadLine());// declare and initialize x(credit) as a variable
+            Console.Write("{0}",x);
+            Console.WriteLine(" is a good course");//print the course as a good course
+           }
+            else if (student == "part time")
+           {
+            Console.WriteLine("how many credits are you taking?");
+            int credit = Convert.ToInt32(Console.ReadLine());
+            if (credit >= 0 && credit <= 6)
+            Console.WriteLine("proceed to the screen");
+        else if (credit > 6)
+        Console.WriteLine("too much unit"); 
+        }
+    
+    else
+    {
+        System.Console.WriteLine("ERROR \nplease cross check your input");
+    }
+    }
+    }
+}
